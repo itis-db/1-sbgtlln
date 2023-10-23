877e4c61bed1e51eb4142dc20a48af916997ec7c
sbgtlln <112934091+sbgtlln@users.noreply.github.com>
2023-10-23 23:08:22 +0300

    Update activity.sql

--- activity.sql
+++ activity.sql
@@ -1,5 +1,5 @@
 ---
-WITH RECURSIVE EntityHierarchy AS (
+WITH RECURSIVE Hierarchy AS (
   SELECT P.ActivityId, A.ActivityTypeId, A.Name, 1 AS Level
   FROM Program P
   INNER JOIN Activity A ON P.ActivityId = A.ActivityId
@@ -8,8 +8,8 @@ WITH RECURSIVE EntityHierarchy AS (
   
   SELECT A.ActivityId, A.ActivityTypeId, A.Name, EH.Level + 1
   FROM Activity A
-  INNER JOIN EntityHierarchy EH ON A.ParentId = EH.ActivityId
+  INNER JOIN Hierarchy EH ON A.ParentId = EH.ActivityId
 )
 SELECT *
-FROM EntityHierarchy
+FROM Hierarchy
 ORDER BY Level, ActivityId;
