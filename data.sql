2c2b54d40fbfb679837ed09f997edbd40f531d03
sbgtlln <112934091+sbgtlln@users.noreply.github.com>
2023-10-23 23:10:39 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,17 +1,14 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+WITH RECURSIVE Hierarchy AS (
+  SELECT P.ActivityId, A.ActivityTypeId, A.Name, 1 AS Level
+  FROM Program P
+  INNER JOIN Activity A ON P.ActivityId = A.ActivityId
+  
+  UNION ALL
+  
+  SELECT A.ActivityId, A.ActivityTypeId, A.Name, EH.Level + 1
+  FROM Activity A
+  INNER JOIN Hierarchy EH ON A.ParentId = EH.ActivityId
+)
+SELECT *
+FROM Hierarchy
+ORDER BY Level, ActivityId;
