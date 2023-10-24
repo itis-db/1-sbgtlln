87b5593666cc948310269d926e06e4bb2b7775ae
sbgtlln <112934091+sbgtlln@users.noreply.github.com>
2023-10-24 12:15:27 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,14 +1,41 @@
-WITH RECURSIVE Hierarchy AS (
-  SELECT P.ActivityId, A.ActivityTypeId, A.Name, 1 AS Level
-  FROM Program P
-  INNER JOIN Activity A ON P.ActivityId = A.ActivityId
-  
-  UNION ALL
-  
-  SELECT A.ActivityId, A.ActivityTypeId, A.Name, EH.Level + 1
-  FROM Activity A
-  INNER JOIN Hierarchy EH ON A.ParentId = EH.ActivityId
-)
-SELECT *
-FROM Hierarchy
-ORDER BY Level, ActivityId;
+INSERT INTO ActivityType (ActivityTypeId, Name)
+VALUES (1, 'Кино'),
+       (2, 'Выставка'),
+       (3, 'Театр');
+
+INSERT INTO Activity (ActivityId, ParentId, ActivityTypeId, Name, AreaId)
+VALUES (1, NULL, 1, 'Фортуна', 1),
+       (2, 1, 2, 'Современное искуство', 2),
+       (3, 1, 2, 'Лебединое озеро', 3),
+       (4, 2, 1, 'Форсаж', 1),
+       (5, 2, 3, 'Ван Гог', 3),
+       (6, NULL, 3, 'Щелкунчик', 2);
+
+INSERT INTO Program (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (6);
+
+INSERT INTO SubProgram (ActivityId)
+VALUES (4),
+       (5);
+
+INSERT INTO Project (ActivityId)
+VALUES (2),
+       (3),
+       (4),
+       (5);
+
+INSERT INTO Contract (ActivityId, AreaId)
+VALUES (3, 1),
+       (4, 2),
+       (6, 3);
+
+INSERT INTO Point (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (4),
+       (5),
+       (6);
